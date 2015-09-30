d668e35083ad3d1c66c6d9bb4f5b61a1ced9bd11
Carrot Zhang <z_p_cing@163.com>
2015-09-30 11:40:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,4 @@ http.createServer(function (request, response) {
 }).listen(5000)
 
 console.log('Node app is running at localhost:' + 5000 + '/');
+console.log('>>>>>>>> webhook test <<<<<<<<<<');
